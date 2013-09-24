20c418bba16a72dc612bf0da5ac77912121a7aae
kevinroseit <kevinroseit@gmail.com>
2013-09-23 23:22:31 -0400

    Huge change only latest and greatest code worth looking at is up

--- Multi-Threading/README.txt
+++ Multi-Threading/README.txt
@@ -1,11 +1,40 @@
-Hello, welcome and g'day!
+---Hello, welcome and g'day!---
 
-Today we have a delicious multi threading program utilizing Posix.  This code should be pretty portable using threads and multiple muti (mutex's?). 
+Today we have a delicious multi threading program utilizing Posix.  
+This code should be pretty portable using threads and multiple muti (mutex's?). 
 
-The main idea for the program is this..
+---The main idea for the program is this:
 
-main waits till i = 10 then quits
-
+main waits till int i = 100 then quits. 
+main will not add to i.
 There are two threads that loop simultaneously one adds 1 to i and another adds 3 to i.
+You can see the output of the threads as they work in parallel!
+It works.. about as well as a rusty pair of pliers....
+Hopefully for large scale applicatoins, like games, threading will increase the performance.
+I have not yet measured the difference between this parallel thread program and a single thread program incrementing i by 3 the whole time.
+Though It would be cool to see the speed and performance differences.
 
-It works.. about as well as a rusty pair of pliers, waiting for a nice lube job.
+
+---Problems:
+
+**Importance = 10** Can't figure out this 'returns 3' problem.
+Researched some of it and found maybe it's because I don't catch any of the exceptions that try_lock throws?
+
+---Get working:
+
+**Importance = 5** Next I would like to get passing objects to threads. 
+I have not researched this at all.
+
+**Importance = 3** I don't like global variable just hanging around. 
+I would like to keep everythign neat and organized in headers and in main etc.
+This is more of like an up keep to make sure lazy code and bad code does not stick around.
+
+**Importance = 2** Start work on some better documentation, not just for this program but for the game as well.
+Maybe come up with a .pdf, examples and ooh pretty pictures, (making this stuff easier and friendlier to deal with, pretty much).
+Reminds me, what is Ian upto? See if he would like to help out.
+Also maybe put some of this stuff up on the git site?
+
+---Other:
+
+Ignore what the pre-amble says in main.cpp. 
+Right now I am working on very basic threading experiments. 
--- Multi-Threading/main.cpp
+++ Multi-Threading/main.cpp
@@ -11,8 +11,7 @@
 #include <mutex>
 //I will not use namespaces, I do not preffer to use namespaces.
 
-std::mutex iLock,jLock;//inputLock,stepLock,drawLock;
-bool alive = true;
+std::mutex iLock,jLock;
 int i;
 
 void inputLoop();
@@ -20,34 +19,43 @@ void stepLoop();
 
 void inputLoop(){
     //int dealy = 10;
-    while(1){
-        int x = try_lock(iLock,jLock);
-        if(x){
+    bool alive = true;
+    while(alive){
+        int x = try_lock(iLock,jLock);//try_lock needs at least 2 objects
+        if(x < 0){//locked all objects
             if(i < 97){
                 i+=3;
-                std::cout << "i = " << i << "    inputLoop+3" << std::endl;
+                std::cout << "i = " << i << "    inputLoop+3" << std::endl;//locks on cout ensure a clean command line prompt, else cout prints all over the place
+                iLock.unlock();//unlock when ever you're done with stuff that can only be safely accessed by things one at a time
+                jLock.unlock();
+            }else{
+                alive = false;//end loop and consequently the thread
                 iLock.unlock();
                 jLock.unlock();
             }
-        }else{
-            //std::cout << "inputLoop failed lock" << std::endl;
-        }
+        }//else{
+            //std::cout << "inputLoop failed lock" << std::endl;//obviously causes cout problems...
+        //}
     }
 
 }
 
 void stepLoop(){
-    //in delay = 10;
-    while(1){
-        if(try_lock(iLock,jLock)){
-            if(i < 99){
+    //int delay = 10;
+    bool alive = true;
+    while(alive){
+        int x = try_lock(iLock,jLock);
+        if(x < 0){
+            if(i < 100){
                 i++;
                 std::cout << "i = " << i <<  "    stepLoop+1" << std::endl;
                 iLock.unlock();
                 jLock.unlock();
+            }else{
+                alive = false;
+                iLock.unlock();
+                jLock.unlock();
             }
-        }else{
-            //std::cout << "stepLoop failed lock" << std::endl;
         }
     }
 }
@@ -58,32 +66,31 @@ void stepLoop(){
 
 int main(int argC, char *argV[]){
 
+    bool alive = true;
     i=0;
 
     std::thread inputThread(inputLoop);
     std::thread stepThread(stepLoop);
     //std::thread drawThread(drawLoop);
 
-    /*while(1){//keep joining thread even if failed
-        if(drawThread.joinable()){
-            drawThread.join();
-            break;
-        }else{
-            std::cout << "Did not join thread." << std::endl;
-        }
-    }*/
-
     while(alive){//wait till 100 then end program
-        if(try_lock(iLock,jLock)){
-            if(i > 10){
-                alive = false;
+        int x = try_lock(iLock,jLock);//CPlusPlus.com recommended using try_lock as such, not sure what the performance difference is if not using variable storage
+        if(x < 0){
+            if(i > 99){
                 std::cout << "Don't worry it's actually counting ----------------" << std::endl;
+                iLock.unlock();
+                jLock.unlock();
+                alive = false;
             }else{
                 iLock.unlock();
                 jLock.unlock();
             }
+        }else{
+            for(int y = 0; y < 10; y++);//this would try to offset this from the other thread cycle, not sure if it does anything or if it's needed.
         }
     }
+
+    for(int y = 0; y < 200; y++);//this is here to let all threads catch up and die off, not sure if it's needed or not
 
     return 0;
 }
