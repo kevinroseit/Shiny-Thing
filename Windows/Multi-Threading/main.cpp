1d24d7d0583aa7a2794ce2ab95ad104112609b04
kevinroseit <kevinroseit@gmail.com>
2013-09-30 07:53:22 -0400

    Now includes use of futures... Futures are not yet working.

--- Windows/Multi-Threading/main.cpp
+++ Windows/Multi-Threading/main.cpp
@@ -1,3 +1,4 @@
+///\/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///\/There will be 3 threads running in parallel.
 ///\/The main thread will call inputThread and stepthread.
 ///\/The main thread will then enter a loop and asychronously (I may have to make this sync. not sure yet)
@@ -7,25 +8,47 @@
 ///\/The purpose of this project is to learn more about threads, synchonicity and locks. (maybe futures and atomics too).
 ///\/Copyright© September 24, 2013
 ///\/Do not use without the expressed writen consent of the **Insert Team Name** Team
-#include <iostream>
-#include <thread>
-#include <mutex>
-#include <exception>
-//I will not use namespaces, I do not preffer to use namespaces.
+///\/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+//Includes
+#include <iostream>//std::cout, std::endl
+#include <thread>//std::thread
+#include <mutex>//std::mutex
+#include <exception>//exit, std::exception
+#include <future>//std::promise, std::future
+#include <functional>//std::ref
+//I will not use namespaces, I do not preffer to use namespaces.
+//End Includes
+
+
+//Globals
 std::mutex iLock,jLock;
 int i;
+//End Globals
 
-void inputLoop();
+
+//Function Prototypes
+void inputLoop(std::future<int> localFuture);
 void stepLoop();
+//End Function Prototypes
 
-void inputLoop(){
+
+//Function Definitions
+void inputLoop(std::future<int> localFuture){
     //int dealy = 10;
+    int test = localFuture.get();
     bool alive = true;
+
     while(alive){
 
+        if(test > 0){
+            alive = false;
+            break;
+        }
+
         try{
-            throw try_lock(iLock,jLock);//CPlusPlus.com recommended using try_lock as such, not sure what the performance difference is if not using variable storage
+            throw try_lock(iLock,jLock);
         }catch (int x){//handle exception
             switch (x){
             case -1:
@@ -39,6 +62,7 @@ void inputLoop(){
                     iLock.unlock();
                     jLock.unlock();
                }
+               break;
             case 0://iLock failed
 
                break;
@@ -49,26 +73,7 @@ void inputLoop(){
                 break;
             }
         }
-        /*
-        int x = try_lock(iLock,jLock);//try_lock needs at least 2 objects
-        if(x < 0){//locked all objects
-            if(i < 97){
-                i+=3;
-                std::cout << "i = " << i << "    inputLoop+3" << std::endl;//locks on cout ensure a clean command line prompt, else cout prints all over the place
-                iLock.unlock();//unlock when ever you're done with stuff that can only be safely accessed by things one at a time
-                jLock.unlock();
-            }else{
-                alive = false;//end loop and consequently the thread
-                iLock.unlock();
-                jLock.unlock();
-            }
-        }//else{
-            //std::cout << "inputLoop failed lock" << std::endl;//obviously causes cout problems...
-        //}
-        */
     }
-    //std::terminate();//apparently this terminates the whole program...
-
 }
 
 void stepLoop(){
@@ -77,7 +82,7 @@ void stepLoop(){
     while(alive){
 
         try{
-            throw try_lock(iLock,jLock);//CPlusPlus.com recommended using try_lock as such, not sure what the performance difference is if not using variable storage
+            throw try_lock(iLock,jLock);
         }catch (int x){
             switch (x){
             case -1:
@@ -91,6 +96,7 @@ void stepLoop(){
                     iLock.unlock();
                     jLock.unlock();
                }
+               break;
             case 0://iLock failed
 
                break;
@@ -101,42 +107,32 @@ void stepLoop(){
                 break;
             }
         }
-        /*
-        int x = try_lock(iLock,jLock);
-        if(x < 0){
-            if(i < 100){
-                i++;
-                std::cout << "i = " << i <<  "    stepLoop+1" << std::endl;
-                iLock.unlock();
-                jLock.unlock();
-            }else{
-                alive = false;
-                iLock.unlock();
-                jLock.unlock();
-            }
-        }*/
     }
-    //std::terminate();//apparently this termiantes the whole program...
 }
+//End Function Definitions
 
-//int drawLoop(){
-    //stuff
-//}
 
+///\/********Main************//////////
+///\/Pass an int to the program to set i to something other than 1
+///\/Pass a string has not yet been handled but I would like it to do something like set debug mode or God mode etc.
+///\/********Main************/////////
 int main(int argC, char *argV[]){
 
+    //i=0;
+    i = argC;
 
-    i=0;
+    std::promise<int> iPromise;//create promise
+    std::future<int> iFuture = iPromise.get_future();//create future and attach promise to future
 
-    std::thread inputThread(inputLoop);
+    iPromise.set_value(0);
+
+    std::thread inputThread(inputLoop, std::ref(iFuture));
     std::thread stepThread(stepLoop);
-    //std::thread drawThread(drawLoop);
 
     bool alive = true;
     while(alive){//wait till 100 then end program
-        //int x = try_lock(iLock,jLock);//CPlusPlus.com recommended using try_lock as such, not sure what the performance difference is if not using variable storage
         try{
-           throw try_lock(iLock,jLock);//CPlusPlus.com recommended using try_lock as such, not sure what the performance difference is if not using variable storage
+           throw try_lock(iLock,jLock);
         }catch (int x){
             switch (x){
             case -1:
@@ -145,64 +141,54 @@ int main(int argC, char *argV[]){
                     iLock.unlock();
                     jLock.unlock();
                     alive = false;
-               }else{
+                    break;//breaks switch not while
+               }
+               if(i > 950){
+                    iPromise.set_value(10);
                     iLock.unlock();
                     jLock.unlock();
+                    break;//breaks switch not while
                }
-            case 0:
+               iLock.unlock();
+               jLock.unlock();
+               break;
+            case 0://iLock failed
 
                break;
-            case 1:
+            case 1://jLock failed
 
                 break;
             default:
                 break;
             }
         }
-        /*if(x < 0){
-            if(i > 99){
-                std::cout << "Don't worry it's actually counting ----------------" << std::endl;
-                iLock.unlock();
-                jLock.unlock();
-                alive = false;
-            }else{
-                iLock.unlock();
-                jLock.unlock();
-            }
-        }else{
-            throw 1;
-            for(int y = 0; y < 10; y++);//this would try to offset this from the other thread cycle, not sure if it does anything or if it's needed.
-        }*/
     }
 
-    try{//sync and close theads
+    try{/////////////////////////////////////////sync and close theads
         throw inputThread.joinable();
     }catch (bool x){
         std::cout << "inputThread.joinable returned" << x << std::endl;
         if(x){
-            std::cout << "Still alive" << std::endl;
+            std::cout << "Joining to Exit Thread" << std::endl;
             inputThread.join();
-            //std::terminate();// this might be terminating main thread... not the thread I want
-            //std::thread ~inputThread;
         }else{
             exit(1000);
         }
     }
 
-    try{//sync and close threads
+    try{/////////////////////////////////////////sync and close threads
         throw stepThread.joinable();
     }catch (bool x){
+        std::cout << "stepThread.joinable returned" << x << std::endl;
         if(x){
+            std::cout << "Joining to Exit Thread" << std::endl;
             stepThread.join();
-            //std::terminate();// this might be terminating main thread... not the thread I want
-            //std::thread ~stepThread;
         }else{
             exit(1000);
         }
     }
-    //for(int y = 0; y < 200; y++);//this is here to let all threads catch up and die off, not sure if it's needed or not
 
-
-
+    //End of Program
     return 0;
+    //End of Program
 }
